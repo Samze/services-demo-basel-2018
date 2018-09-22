97ee47cd0157f77ee8f54a5ad6842e76f14787de
Sam Gunaratne <sgunaratne@pivotal.io>
2018-09-22 16:33:09 +0100

    update cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -ex
 
 unbind() {
-  cf unbind-service $1 pubsub
-  while cf service pubsub | grep -q $1; do
+  cf unbind-service $1 $2
+  while cf service $2 | grep -q $1; do
     sleep 2
   done
 }
 
-unbind web-app
-unbind worker-app
+# broker does not support per service unbinds in parallel
+unbind web-app pubsub
+unbind worker-app pubsub
+
+unbind web-app postgresql
+unbind worker-app postgresql
 
 cf d web-app -f
 cf d worker-app -f
